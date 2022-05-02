9f00118ec3ee8262f1c876c62cf487b4d1bacaf3
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-02 17:43:00 +0530

    [Ronit]Added : Create employee payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,9 @@
 create database payroll_service_DB;
 use payroll_service_DB;
+------------UC-2--------------
+CREATE TABLE employee_payroll_DB (
+Id int identity(1,1) Primary key,
+Name varchar(200),
+Salary float,
+Startdate datetime
+);
