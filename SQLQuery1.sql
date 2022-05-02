697b1acab99e69b0992b1aaa5ca25d0f2f76c51c
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-02 17:57:33 +0530

    [Ronit]Added : insert into employee payroll

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,3 +7,8 @@ Name varchar(200),
 Salary float,
 Startdate datetime
 );
+------------UC-3--------------
+INSERT INTO employee_payroll_DB (Name,Salary,Startdate) 
+VALUES ('Ronit',25000,'1999-01-04'),('Dibya',20000,'2002-11-06'),
+('KD',24000,'2010-01-01'),('Abhi',20000,'2022-02-21');
+
